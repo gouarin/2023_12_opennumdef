5b68592cbcc5c3cc12384bd24873539d2f544075
Loic Gouarin <loic.gouarin@gmail.com>
2023-11-27 17:21:27 +0100

    fix

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js'
 import RevealMath from 'reveal.js/plugin/math/math.js'
-// import RevealMenu from './plugin.js'
-import RevealMenu from 'reveal.js-menu/plugin.js'
+import RevealMenu from './plugin.js'
+// import RevealMenu from 'reveal.js-menu/plugin.js'
 
 var url = "2023_12_01_opennumdef";
 
